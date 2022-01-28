c640ac6f108fcf7531b4a7fe54dea068f54c19d4
avtandil0 <avtandilzenaishvili@gmail.com>
2022-01-28 16:15:06 +0400

    ექსელის გენერაცია

--- PayrollServer/Controllers/CalculationController.cs
+++ PayrollServer/Controllers/CalculationController.cs
@@ -4,6 +4,8 @@ using Entities.FilterModels;
 using ExcelLibrary.SpreadSheet;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 using PayrollServer.Models;
 using PayrollServer.Models.DTOs;
 using System;
@@ -59,28 +61,92 @@ namespace PayrollServer.Controllers
         public FileStreamResult GenerateExcel([FromQuery] CalculationFilter calculationFilter)
         {
 
-            Workbook workbook = new Workbook();
+            //Workbook workbook = new Workbook();
 
-            Worksheet worksheet = new Worksheet("First Sheet");
-            worksheet.Cells[0, 1] = new Cell((short)1);
-            worksheet.Cells[2, 0] = new Cell(9999999);
-            worksheet.Cells[3, 3] = new Cell((decimal)3.45);
-            worksheet.Cells[2, 2] = new Cell("Text string");
-            worksheet.Cells[2, 4] = new Cell("Second string");
-            worksheet.Cells[4, 0] = new Cell(32764.5, "#,##0.00");
-            worksheet.Cells[5, 1] = new Cell(DateTime.Now, @"YYYY-MM-DD"); worksheet.Cells.ColumnWidth[0, 1] = 3000;
-            workbook.Worksheets.Add(worksheet);
+            //Worksheet worksheet = new Worksheet("First Sheet");
+            //worksheet.Cells[0, 1] = new Cell((short)1);
+            //worksheet.Cells[2, 0] = new Cell(9999999);
+            //worksheet.Cells[3, 3] = new Cell((decimal)3.45);
+            //worksheet.Cells[2, 2] = new Cell("Text string");
+            //worksheet.Cells[2, 4] = new Cell("Second string");
+            //worksheet.Cells[4, 0] = new Cell(32764.5, "#,##0.00");
+            //worksheet.Cells[5, 1] = new Cell(DateTime.Now, @"YYYY-MM-DD"); worksheet.Cells.ColumnWidth[0, 1] = 3000;
+            //workbook.Worksheets.Add(worksheet);
 
 
 
-            //_logger.LogInfo($"Created new Employee.");
+            ////_logger.LogInfo($"Created new Employee.");
 
-            var stream = new MemoryStream();
-            workbook.SaveToStream(stream);
+            //var stream = new MemoryStream();
+            //workbook.SaveToStream(stream);
+
+            //var mimeType = "Application/vnm.ms-excel";
+            //var fileStreamResult = new FileStreamResult(stream, mimeType);
+            ////fileStreamResult.FileDownloadName = string.Format("To Bank BOG {0:yyyy_MM_dd_HH_mm_ss}.xlsx", DateTime.Now);
+
+            //stream.Seek(0, SeekOrigin.Begin);
+
+
+            //return fileStreamResult;
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            ExcelPackage excel = new ExcelPackage();
+
+            // name of the sheet
+            var workSheet = excel.Workbook.Worksheets.Add("Sheet1");
+
+            // setting the properties
+            // of the work sheet 
+            workSheet.TabColor = System.Drawing.Color.Black;
+            workSheet.DefaultRowHeight = 12;
+
+            // Setting the properties
+            //// of the first row
+            //workSheet.Row(1).Height = 20;
+            ////workSheet.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            //workSheet.Row(1).Style.Font.Bold = true;
+
+            // Header of the Excel sheet
+            workSheet.Cells[1, 1].Value = "xelfasi";
+            workSheet.Cells[2, 1].Value = "angariSi";
+            workSheet.Cells[2, 2].Value = " biki";
+            workSheet.Cells[2, 3].Value = "dasaxeleba";
+            workSheet.Cells[2, 4].Value = "daniSnuleba";
+            workSheet.Cells[2, 5].Value = "tanxa";
+
+            // Inserting the article data into excel
+            // sheet by using the for each loop
+            // As we have values to the first row 
+            // we will start with second row
+            int recordIndex = 3;
+
+            var employees = _repository.Employee.GetCalculationByFilter(calculationFilter);
+
+            foreach (var employee in employees)
+            {
+                workSheet.Cells[recordIndex, 1].Value = employee.BankAccountNumber;
+                workSheet.Cells[recordIndex, 2].Value = "TBCBGE22"; // biki
+                workSheet.Cells[recordIndex, 3].Value = String.Format("{0} {1}", employee.FirstName, employee.LastName);
+                workSheet.Cells[recordIndex, 4].Value = "xelfasi"; //  daniSnuleba
+                workSheet.Cells[recordIndex, 5].Value = employee.Calculations.Sum(r => r.Net);
+                recordIndex++;
+            }
+
+            // By default, the column width is not 
+            // set to auto fit for the content
+            // of the range, so we are using
+            // AutoFit() method here. 
+            workSheet.Column(1).AutoFit();
+            workSheet.Column(2).AutoFit();
+            workSheet.Column(3).AutoFit();
+
+
+            var stream = new MemoryStream(excel.GetAsByteArray());
 
             var mimeType = "Application/vnm.ms-excel";
             var fileStreamResult = new FileStreamResult(stream, mimeType);
-            //fileStreamResult.FileDownloadName = string.Format("To Bank BOG {0:yyyy_MM_dd_HH_mm_ss}.xlsx", DateTime.Now);
+            fileStreamResult.FileDownloadName = string.Format("To Bank BOG {0:yyyy_MM_dd_HH_mm_ss}.xlsx", DateTime.Now);
 
             stream.Seek(0, SeekOrigin.Begin);
 
