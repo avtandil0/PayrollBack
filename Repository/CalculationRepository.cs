7d96f044291d3ae0b416da0e33e730402b30a6c9
avtandil0 <avtandilzenaishvili@gmail.com>
2022-02-16 16:33:12 +0400

    update კალკულაციის ცაწერა

--- Repository/CalculationRepository.cs
+++ Repository/CalculationRepository.cs
@@ -61,59 +61,63 @@ namespace Repository
                         }
                     }
 
-                    var coefficient = RepositoryContext.Coefficients.Where(r => r.Id == component.CoefficientId && r.DateDeleted == null).FirstOrDefault();
-
-                    Calculation calculation = new Calculation();
-                    calculation.Id = Guid.NewGuid();
-                    calculation.CalculationDate = calculationDate;
-                    calculation.DateCreated = DateTime.Now;
-
-                    calculation.EmployeeId = emp.Id;
-                    calculation.EmployeeComponentId = empComp.Id;
-                    calculation.PayrollYear = calculationDate.Year;
-                    calculation.PayrollMonth = calculationDate.Month;
-                    calculation.SchemeTypeId = empComp.SchemeTypeId;
-                    calculation.RemainingGraceAmount = 0;
-
-                    if (emp.SchemeTypeId == (int)SchemeTypeEnum.Standart)
-                    {
-                        calculation.Gross = empComp.Amount * (decimal)coefficient.Sgross;
-                        calculation.Net = empComp.Amount * (decimal)coefficient.Snet;
-                        calculation.Paid = empComp.Amount * (decimal)coefficient.Spaid;
-                        calculation.PensionTax = empComp.Amount * (decimal)coefficient.Spension;
-                        calculation.IncomeTax = empComp.Amount * (decimal)coefficient.SincomeTax;
-                    }
-
-                    if (emp.SchemeTypeId == (int)SchemeTypeEnum.Pension)
-                    {
-                        calculation.Gross = empComp.Amount * (decimal)coefficient.Pgross;
-                        calculation.Net = empComp.Amount * (decimal)coefficient.Pnet;
-                        calculation.Paid = empComp.Amount * (decimal)coefficient.Ppaid;
-                        calculation.PensionTax = empComp.Amount * (decimal)coefficient.Ppension;
-                        calculation.IncomeTax = empComp.Amount * (decimal)coefficient.PincomeTax;
-                    }
-
-                    if (emp.RemainingGraceAmount > 0)
-                    {
-                        var rem = emp.RemainingGraceAmount - (calculation.Gross - calculation.PensionTax);
-                        if (rem > 0)
-                        {
-                            emp.RemainingGraceAmount = rem;
-                            calculation.RemainingGraceAmount = rem;
-                            calculation.IncomeTax = 0;
-                            calculation.Net = calculation.Gross - calculation.PensionTax;
-                        }
-                        else
-                        {
-                            calculation.IncomeTax = (decimal)((calculation.Gross - calculation.PensionTax - emp.RemainingGraceAmount) / 5);
-                            calculation.Net = calculation.Gross - calculation.PensionTax - calculation.IncomeTax;
-                            emp.RemainingGraceAmount = 0;
-                            calculation.RemainingGraceAmount = 0;
-                        }
-
-                    }
+                    var calculation = GetCalculationObject(calculationDate, component.CoefficientId, emp, empComp);
 
                     Create(calculation);
+
+                    //var coefficient = RepositoryContext.Coefficients.Where(r => r.Id == component.CoefficientId && r.DateDeleted == null).FirstOrDefault();
+
+                    //Calculation calculation = new Calculation();
+                    //calculation.Id = Guid.NewGuid();
+                    //calculation.CalculationDate = calculationDate;
+                    //calculation.DateCreated = DateTime.Now;
+
+                    //calculation.EmployeeId = emp.Id;
+                    //calculation.EmployeeComponentId = empComp.Id;
+                    //calculation.PayrollYear = calculationDate.Year;
+                    //calculation.PayrollMonth = calculationDate.Month;
+                    //calculation.SchemeTypeId = empComp.SchemeTypeId;
+                    //calculation.RemainingGraceAmount = 0;
+
+                    //if (emp.SchemeTypeId == (int)SchemeTypeEnum.Standart)
+                    //{
+                    //    calculation.Gross = empComp.Amount * (decimal)coefficient.Sgross;
+                    //    calculation.Net = empComp.Amount * (decimal)coefficient.Snet;
+                    //    calculation.Paid = empComp.Amount * (decimal)coefficient.Spaid;
+                    //    calculation.PensionTax = empComp.Amount * (decimal)coefficient.Spension;
+                    //    calculation.IncomeTax = empComp.Amount * (decimal)coefficient.SincomeTax;
+                    //}
+
+                    //if (emp.SchemeTypeId == (int)SchemeTypeEnum.Pension)
+                    //{
+                    //    calculation.Gross = empComp.Amount * (decimal)coefficient.Pgross;
+                    //    calculation.Net = empComp.Amount * (decimal)coefficient.Pnet;
+                    //    calculation.Paid = empComp.Amount * (decimal)coefficient.Ppaid;
+                    //    calculation.PensionTax = empComp.Amount * (decimal)coefficient.Ppension;
+                    //    calculation.IncomeTax = empComp.Amount * (decimal)coefficient.PincomeTax;
+                    //}
+
+                    //if (emp.RemainingGraceAmount > 0)
+                    //{
+                    //    var rem = emp.RemainingGraceAmount - (calculation.Gross - calculation.PensionTax);
+                    //    if (rem > 0)
+                    //    {
+                    //        emp.RemainingGraceAmount = rem;
+                    //        calculation.RemainingGraceAmount = rem;
+                    //        calculation.IncomeTax = 0;
+                    //        calculation.Net = calculation.Gross - calculation.PensionTax;
+                    //    }
+                    //    else
+                    //    {
+                    //        calculation.IncomeTax = (decimal)((calculation.Gross - calculation.PensionTax - emp.RemainingGraceAmount) / 5);
+                    //        calculation.Net = calculation.Gross - calculation.PensionTax - calculation.IncomeTax;
+                    //        emp.RemainingGraceAmount = 0;
+                    //        calculation.RemainingGraceAmount = 0;
+                    //    }
+
+                    //}
+
+
                 }
             }
 
@@ -165,31 +169,30 @@ namespace Repository
 
             var currentTime = DateTime.Now;
 
-            foreach (var item in employees)
+            foreach (var employee in employees)
             {
-                var person = paidHelper.Persons.Where(r => r.BankAccountNumber.Contains(item.BankAccountNumber)).FirstOrDefault();
+                var person = paidHelper.Persons.Where(r => r.BankAccountNumber.Contains(employee.BankAccountNumber)).FirstOrDefault();
 
                 if(person != null)
                 {
                     var employeeComponent = new EmployeeComponent();
-                    var calculation = new Calculation();
 
                     employeeComponent.Id = Guid.NewGuid();
                     employeeComponent.DateCreated = currentTime;
                     employeeComponent.StartDate = currentTime;
                     employeeComponent.EndDate = currentTime;
-                    employeeComponent.EmployeeId = item.Id;
+                    employeeComponent.EmployeeId = employee.Id;
                     employeeComponent.ComponentId = paidHelper.ComponentID;
                     employeeComponent.Amount = Convert.ToDecimal(person.Amount);
                     employeeComponent.Currency = "GEL";
 
 
-                  
 
-                    calculation.Id = Guid.NewGuid();
-                    calculation.DateCreated = currentTime;
-                    calculation.EmployeeId = item.Id;
+                    var component = RepositoryContext.Components.Where(r => r.Id == paidHelper.ComponentID).FirstOrDefault();
 
+                    var calculation = GetCalculationObject(paidHelper.PaidDate, component.CoefficientId, employee, employeeComponent);
+
+                    Create(calculation);
 
                     RepositoryContext.EmployeeComponents.Add(employeeComponent);
                 }
@@ -207,9 +210,62 @@ namespace Repository
             throw new NotImplementedException();
         }
 
-        public Calculation GetCalculationObject(Coefficient coefficient)
+        public Calculation GetCalculationObject(DateTime calculationDate, Guid? coefficientId, Employee employee, EmployeeComponent empComp)
         {
-            throw new NotImplementedException();
+
+            var coefficient = RepositoryContext.Coefficients.Where(r => r.Id == coefficientId && r.DateDeleted == null).FirstOrDefault();
+
+            Calculation calculation = new Calculation();
+            calculation.Id = Guid.NewGuid();
+            calculation.CalculationDate = calculationDate;
+            calculation.DateCreated = DateTime.Now;
+
+            calculation.EmployeeId = employee.Id;
+            calculation.EmployeeComponentId = empComp.Id;
+            calculation.PayrollYear = calculationDate.Year;
+            calculation.PayrollMonth = calculationDate.Month;
+            calculation.SchemeTypeId = empComp.SchemeTypeId;
+            calculation.RemainingGraceAmount = 0;
+
+            if (employee.SchemeTypeId == (int)SchemeTypeEnum.Standart)
+            {
+                calculation.Gross = empComp.Amount * (decimal)coefficient.Sgross;
+                calculation.Net = empComp.Amount * (decimal)coefficient.Snet;
+                calculation.Paid = empComp.Amount * (decimal)coefficient.Spaid;
+                calculation.PensionTax = empComp.Amount * (decimal)coefficient.Spension;
+                calculation.IncomeTax = empComp.Amount * (decimal)coefficient.SincomeTax;
+            }
+
+            if (employee.SchemeTypeId == (int)SchemeTypeEnum.Pension)
+            {
+                calculation.Gross = empComp.Amount * (decimal)coefficient.Pgross;
+                calculation.Net = empComp.Amount * (decimal)coefficient.Pnet;
+                calculation.Paid = empComp.Amount * (decimal)coefficient.Ppaid;
+                calculation.PensionTax = empComp.Amount * (decimal)coefficient.Ppension;
+                calculation.IncomeTax = empComp.Amount * (decimal)coefficient.PincomeTax;
+            }
+
+            if (employee.RemainingGraceAmount > 0)
+            {
+                var rem = employee.RemainingGraceAmount - (calculation.Gross - calculation.PensionTax);
+                if (rem > 0)
+                {
+                    employee.RemainingGraceAmount = rem;
+                    calculation.RemainingGraceAmount = rem;
+                    calculation.IncomeTax = 0;
+                    calculation.Net = calculation.Gross - calculation.PensionTax;
+                }
+                else
+                {
+                    calculation.IncomeTax = (decimal)((calculation.Gross - calculation.PensionTax - employee.RemainingGraceAmount) / 5);
+                    calculation.Net = calculation.Gross - calculation.PensionTax - calculation.IncomeTax;
+                    employee.RemainingGraceAmount = 0;
+                    calculation.RemainingGraceAmount = 0;
+                }
+
+            }
+
+            return calculation;
         }
     }
 }
